799bb0c1d85e063d30c2471f21ba36fa92ae30f0
hai.h.nguyen <hai.h.nguyen@LGDN13261-W7C02.logigear.com>
2015-06-30 17:54:44 +0700

    test Hub

--- Selenium_Group5/src/pages/TestHai.java
+++ Selenium_Group5/src/pages/TestHai.java
@@ -1,7 +1,12 @@
 package pages;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -12,10 +17,21 @@ import common.config;
 public class TestHai {
 
 	public static void main(String[] args) {
+		URL hubUrl;
+		try {
+			hubUrl = new URL("http://localhost:4444/wd/hub");
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			RemoteWebDriver driver = new RemoteWebDriver(hubUrl, capabilities);			
+			driver.get("http://www.google.com");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		String menu = "hoanghai/a";
-		String[] arr = menu.split("/");
-		System.out.println(arr.length);
+		
+//		String menu = "hoanghai/a";
+//		String[] arr = menu.split("/");
+//		System.out.println(arr.length);
 		
 	}
 	
